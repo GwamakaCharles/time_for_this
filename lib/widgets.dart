90140a1c259b7f80e2729e31646af2c66b081f33
Gwamaka Charles <gwaxart19@gmail.com>
2020-08-18 20:51:06 +0300

    edited the readme

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -24,7 +24,7 @@ class ProductivityButton extends StatelessWidget {
       ),
       onPressed: this.onPressed,
       color: this.color,
-      minWidth: this.size,
+      minWidth: (this.size != null) ? this.size : 0,
     );
   }
 }
@@ -32,19 +32,16 @@ class ProductivityButton extends StatelessWidget {
 class SettingsButton extends StatelessWidget {
   final Color color;
   final String text;
-  final double size;
   final int value;
   final String setting;
   final CallbackSetting callback;
-  SettingsButton(this.color, this.text, this.size, this.value, this.setting,
-      this.callback);
+  SettingsButton(this.color, this.text, this.value, this.setting,this.callback);
   @override
   Widget build(BuildContext context) {
     return MaterialButton(
       child: Text(this.text, style: TextStyle(color: Colors.white)),
       onPressed: () => this.callback(this.setting, this.value),
       color: this.color,
-      minWidth: this.size,
     );
   }
 }
